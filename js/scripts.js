cc4a75883e00fc8f1e39d70c01e64e4ee2baff6a
BlackMetalzCode <joselmlftw@gmail.com>
2016-10-09 05:12:18 -0430

    initial commit

---
+++ js/scripts.js
@@ -0,0 +1,37 @@
+$(document).ready(function(){
+
+
+// Smooth scroll
+$(function() {
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
+
+// Smooth scroll ends.
+
+
+// Waypoints 
+
+$('#ts3').waypoint(function(){
+  $(this).toggleClass($(this).data('animated'));
+},
+{ offset: '90%'
+});
+
+
+
+
+}); // Ready
+
+
+
